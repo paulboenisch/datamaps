5572ebdf473905128c7e58374aa48f46ad33eeb7
Paul Bönisch <paul.boenisch@umwerk.net>
2018-03-06 14:46:42 +0100

    adding kosovo id

--- dist/datamaps.all.js
+++ dist/datamaps.all.js
@@ -382,6 +382,9 @@
                    case "CHL":
                        originXY = self.latLngToXY(-33.448890, -70.669265);
                        break;
+                   case "HRV":
+                       originXY = self.latLngToXY(45.815011, 15.981919);
+                       break;
                    case "IDN":
                        originXY = self.latLngToXY(-6.208763, 106.845599);
                        break;
@@ -409,11 +412,14 @@
 
             if (typeof datum.destination === 'string') {
               switch (datum.destination) {
-                     case "CAN":
+                    case "CAN":
                         destXY = self.latLngToXY(56.624472, -114.665293);
                         break;
                     case "CHL":
                         destXY = self.latLngToXY(-33.448890, -70.669265);
+                        break;
+                    case "HRV":
+                        destXY = self.latLngToXY(45.815011, 15.981919);
                         break;
                     case "IDN":
                         destXY = self.latLngToXY(-6.208763, 106.845599);
@@ -1846,7 +1852,7 @@
                 "properties": {
                     "name": "Kosovo"
                 },
-                "id": "-99",
+                "id": "KOS",
                 "arcs": [
                     [-18, 405, 406, 407]
                 ]
@@ -2459,7 +2465,7 @@
                 "properties": {
                     "name": "Somaliland"
                 },
-                "id": "-99",
+                "id": "SOL",
                 "arcs": [
                     [-263, -231, 559, 560]
                 ]
